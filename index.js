449f605ef93353cc8306117bb55a6375b0c40f0b
Graham <thegrahamprice@icloud.com>
2022-07-15 15:39:57 -0600

    commit tree adfcb70d1035f36c4e0998a9135ac642229bf12f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+//How come we don't have to define users or drivers? and what is sought?
+
+function findMatching(nuts, sucked) {
+  return nuts.filter(
+    (fuckboi) => fuckboi.toLowerCase() === sucked.toLowerCase()
+  );
+}
+
+function fuzzyMatch(nuts, Pussy) {
+  return nuts.filter(
+    (fuckboi) => fuckboi.toLowerCase().indexOf(Pussy.toLowerCase()) === 0
+  );
+}
+
+function matchName(nuts, blueStrips) {
+  return nuts.filter((dirt) => dirt.name === blueStrips);
+}
